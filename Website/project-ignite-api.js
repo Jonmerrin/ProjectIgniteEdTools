55be182309644d4bdd6f5b99527e826a089a6bac
Jacob Neumann <Jacob Neumann>
2017-03-26 15:42:33 -0400

    Delete commented-out table example from api

--- Website/project-ignite-api.js
+++ Website/project-ignite-api.js
@@ -131,38 +131,6 @@ var createTable = function(x,y,width, font, size, headContent, content, border=f
 }
 
 
-          // <table class="table table-striped">
-          //   <thead>
-          //     <tr>
-          //       <th>#</th>
-          //       <th>First Name</th>
-          //       <th>Last Name</th>
-          //       <th>Username</th>
-          //     </tr>
-          //   </thead>
-          //   <tbody>
-          //     <tr>
-          //       <td>1</td>
-          //       <td>Mark</td>
-          //       <td>Otto</td>
-          //       <td>@mdo</td>
-          //     </tr>
-          //     <tr>
-          //       <td>2</td>
-          //       <td>Jacob</td>
-          //       <td>Thornton</td>
-          //       <td>@fat</td>
-          //     </tr>
-          //     <tr>
-          //       <td>3</td>
-          //       <td>Larry</td>
-          //       <td>the Bird</td>
-          //       <td>@twitter</td>
-          //     </tr>
-          //   </tbody>
-          // </table>
-
-
 
 
 // var createTimer = function(time, tag, onEnd){
