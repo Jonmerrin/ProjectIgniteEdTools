cfbd01138e137c13a321dd0295ca87c3edb38e00
kazumap <kazumaparkinson@gmail.com>
2017-05-24 20:01:57 -0400

    Revert "Revert "pushing more changes""
    This reverts commit 8a17444037b6a6bea803c4f5549861d2e9753459.

--- Website/questionscreen.js
+++ Website/questionscreen.js
@@ -5,28 +5,25 @@ var complementColor = "#FF4466"
 
 createImgButton(0, 0, (HEIGHT*3/32)+"px", (HEIGHT*3/32)+"px", undefined, undefined, "homebutton.png", 1);
 
+
+
+createTable(200,200, 1200, "Arial", 30, ["Letter", "Answer"],
+	[
+		["A", "Answer"],
+		["B", "Answer"],
+		["C", "Answer"],
+		["D", "Answer"] 
+	], true, true)
+
+
+
+
 createHeader(600, (HEIGHT/32), "Arial", (HEIGHT/24), "Subject");
 
 createHeader(600, (HEIGHT/8), "Arial", (HEIGHT/20), "Question");
 
 
 
-
-createText(0, 200, "Arial" , 40, "A", true, 20)
-
-createText(0, 300, "Arial" , 40, "B", true, 20)
-
-createText(0, 400, "Arial" , 40, "C", true, 20)
-
-createText(0, 500, "Arial" , 40, "D", true, 20)
-
-createHeader(90, 200, "Arial", 40, "Answer Choice A", true, 20)
-
-createHeader(90, 300, "Arial", 40, "Answer Choice B", true, 20)
-
-createHeader(90, 400, "Arial", 40, "Answer Choice C", true, 20)
-
-createHeader(90, 500, "Arial", 40, "Answer Choice D", true, 20)
 
 
 
